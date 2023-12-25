1bcd77a406192e614e6290349040491c3f99d6b2
Jash-tech <jashsuke@gmail.com>
2023-12-25 10:57:39 +0530

    Data Transformation Created

--- src/mlproject/utils.py
+++ src/mlproject/utils.py
@@ -4,6 +4,7 @@ import pandas as pd
 import pymysql
 
 import numpy as np
+import dill
 
 
 from src.mlproject.logger import logging
@@ -38,6 +39,15 @@ def read_sql_data():
     
 
 
+def save_object(file_path,obj):
+    try:
+        dir_path=os.path.dirname(file_path)
+        os.makedirs(dir_path,exist_ok=True)
 
+        with open (file_path,"wb") as file_obj:
+            dill.dump(obj,file_obj)
+
+    except Exception as e:
+        raise CustomException(e,sys)
 
 
